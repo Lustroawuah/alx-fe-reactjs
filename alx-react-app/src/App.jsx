3fcefbc7ab80471474de5357951448bd8e40cfbb
Lustro Awuah <shineawuah@gmail.com>
2025-02-25 12:32:01 +0000

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,12 +23,13 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello world
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
   )
 }

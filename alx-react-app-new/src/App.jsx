69b6213eec65fd89b816a285cd34b524bc634996
Lustro Awuah <shineawuah@gmail.com>
2025-03-05 20:17:14 +0000

    update

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile' 
+import Counter from ' ./components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,6 +33,7 @@ function App() {
       <Header/>
       <MainContent/>
       <Footer/>
+      <Counter/>
       <div>
        <h1>Welcome to User Profile</h1>
        <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />

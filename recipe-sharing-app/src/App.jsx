9cd7a85dfd3e492090a841e231bfe1ae58757ea0
Lustro Awuah <shineawuah@gmail.com>
2025-03-07 16:18:14 +0000

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
   const [count, setCount] = useState(0)
+ 
 
   return (
     <>
@@ -28,6 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <AddRecipeForm/>
+      <RecipeList/>
     </>
   )
 }

596134c72e5459a2454b73e06bcafc110fcdddf9
Lustro Awuah <shineawuah@gmail.com>
2025-03-16 00:51:58 +0000

    update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
       <div class="bg-gray-100 md:p-8  sm:p-4 md:max-w-sm sm:max-w-xs mx-auto, my-20 rounded-lg shadow-lg" className="user-profile">
-        <img class="rounded-full sm:w-24 h-24, md:w-36 h-36 mx-auto"src="https://via.placeholder.com/150" alt="User" />
+        <img class="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"src="https://via.placeholder.com/150" alt="User" />
         <h1 class="md:text-xl sm:text-lg text-blue-800 my-4 ">John Doe</h1>
         <p class="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
